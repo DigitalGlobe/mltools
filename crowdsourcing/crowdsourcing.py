31601a9f86b1496d972f5cdf7ccd0087672d1be0
Kostas Stamatiou <kostastamatiou@gmail.com>
2016-02-24 11:09:56 -0700

    added confusion_matrix computation function

--- crowdsourcing/crowdsourcing.py
+++ crowdsourcing/crowdsourcing.py
@@ -7,7 +7,9 @@ Contact: kostas.stamatiou@digitalglobe.com
 """
 
 import geojson
+import numpy as np
 from shapely.wkb import loads
+from sklearn.metrics import confusion_matrix
 import tomnodDB as DB
 
 
@@ -196,4 +198,55 @@ def split_geojson(input_file, file_1, file_2, ratio):
 
 	with open(file_2, 'w') as f:
 		geojson.dump(feat_collection_2, f) 	
+
+
+def get_classes(input_file):
+	"""Reads a geojson with class_name property and returns a vector
+	   with all the class names.
+
+	   Args:
+	       input_file (str): Input filename (.geojson extension).
+
+	   Returns:
+	       A list of class names (list).     
+	"""
+
+	# get feature collections
+	with open(input_file) as f:
+	    feature_collection = geojson.load(f)
+
+	features = feature_collection['features']    
+	labels = [feat['properties']['class_name'] for feat in features]
+
+	return labels    
+
+
+def confusion_matrix_two_geojsons(file_1, file_2, class_names):
+	"""
+	Compute confusion matrix to evaluate the accuracy of a classification.
+
+	Args:
+	    file_1 (str): Ground truth filename (.geojson extension)
+	    file_2 (str): Prediction filename (.geojson extension)
+	    class_names (list): List of class names. Each class name is a string.
+
+	Returns:
+	    An integer numpy array C, where C[i,j] is the number of observations 
+	    known to be in group i but predicted to be in group j (numpy array).     
+	"""
+
+	true_classes = get_classes(file_1)
+	pred_classes = get_classes(file_2)
+
+	C = confusion_matrix(true_classes, pred_classes, class_names)
+
+	return C
+
+
+
+
+
+
+
+
 
