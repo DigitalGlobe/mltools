3802ec91030c2b3302eae54d5c9b0b56b52c105c
Kostas Stamatiou <kostastamatiou@gmail.com>
2016-02-26 15:04:47 -0700

    updated

--- mltools/__init__.py
+++ mltools/__init__.py
@@ -4,3 +4,5 @@ from . import json_tools
 from . import pixel_extractors
 
 from pool_detector import pool_detector
+from simple_classifier import simple_classifier
+from simple_lulc import simple_lulc
