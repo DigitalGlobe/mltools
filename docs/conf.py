bf792c41aa1b92667b4d74e15d39f4f6710ae41f
Kostas Stamatiou <kostastamatiou@gmail.com>
2016-02-26 10:24:42 -0700

    added mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,17 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     	html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
+from mock import Mock as MagicMock
+    
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+                return Mock()
+
+MOCK_MODULES = ['numpy', 'geojson', 'gdal', 'psycopg2', 'geojson', ]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
