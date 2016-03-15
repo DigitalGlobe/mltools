e24a60cd5dc3ed6c79de8e57b72a314ffedfac51
Kostas Stamatiou <kostastamatiou@gmail.com>
2016-03-15 13:43:27 -0600

    removed import of outdated modules

--- mltools/__init__.py
+++ mltools/__init__.py
@@ -1,8 +1,5 @@
 from . import crowdsourcing
+from . import data_extractors
 from . import feature_extractors
 from . import json_tools
-from . import pixel_extractors
-
-from pool_detector import pool_detector
-from simple_classifier import simple_classifier
-from simple_lulc import simple_lulc
+from . import polygon_classifier
