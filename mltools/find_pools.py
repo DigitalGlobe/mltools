eb40813356a3230b1e2b092d59ad8fa204d134f6
Kostas Stamatiou <kostastamatiou@gmail.com>
2016-03-15 13:54:25 -0600

    removed find_pools_real

--- mltools/find_pools.py
+++ mltools/find_pools.py
@@ -6,13 +6,9 @@
 
 # This script was used in the adelaide_pools_2016 campaign. 
 
-import geojson
 import json
 import os
 import sys
-
-import crowdsourcing as cr
-import json_tools as jt
 
 from polygon_classifier import PolygonClassifier
 from crowdsourcing import TomnodCommunicator
@@ -21,32 +17,32 @@ from crowdsourcing import TomnodCommunicator
 import warnings
 warnings.filterwarnings("ignore")
 
-# tomnod credentials
 
-
-# get job parameters from json
+# get job parameters
 with open('job.json', 'r') as f:
     job = json.load(f)
 
 schema = job['schema']
 cat_id = job['cat_id']
+image_file = cat_id + '.tif'
 no_train_pools = job['no_train_pools']
 no_train_nopools = job['no_train_nopools']
-max_polys_to_classify = job['max_polygons'] 
-max_poly_area = job['max_area']             # don't consider polys too large 
-algorithm_params = job['algorithm_params'] 
+max_polygons = job['max_polygons']   # max polygons to be classified
+max_area = job['max_area']           # max area in m2
+train_file = cat_id + '_train_real.geojson'
+target_file = cat_id + '_target_real.geojson'
+output_file = cat_id + '_real.geojson'
+algorithm_params = job['algorithm_params']
 
-image_file = cat_id + '.tif'
-train_file = cat_id + '_train.geojson'
-test_file = cat_id + '_test.geojson'
-target_file = cat_id + '_target.geojson'
-output_file = cat_id + '.geojson'
+# get ground truth for pools and no pools 
+print 'Get GT'
 
-print 'Get ground truth'
-
+# get tomnod credentials
 with open('credentials.json', 'r') as f:
     credentials = json.load(f)
 
+# initialize Tomnod communicator class
+crowd = TomnodCommunicator(credentials)
 
 cr.train_geojson(schema, 
                  cat_id, 
@@ -55,7 +51,6 @@ cr.train_geojson(schema,
                  'Swimming pool', 
                  credentials,
                  min_votes = 1)
-
 
 cr.train_geojson(schema, 
                  cat_id, 
@@ -80,7 +75,61 @@ cr.target_geojson(schema,
                   max_area = max_area 
                   )
 
+print 'Train and test model'
+c = PolygonClassifier(algorithm_params)
+c.train('train.geojson')
 
-# run pool detector
-print 'Run detector'
-pcp.main('job.json
+
+#### more stuff, potentially useful for final form of this script
+
+# def main(job_file):
+#     """Runs the simple_lulc workflow.
+
+#        Args:
+#            job_file (str): Job filename (.json, see README of this repo) 
+#     """    
+   
+#     # get job parameters
+#     job = json.load(open(job_file, 'r'))
+#     image_file = job["image_file"]
+#     train_file = job["train_file"]
+#     target_file = job["target_file"]
+#     output_file = job["output_file"]
+#     algo_params = job["params"]       # these are parameters pertinent to the 
+#                                       # algorithm
+    
+#     # Using random forest classifier
+#     n_estimators = algo_params["n_estimators"]
+#     oob_score = algo_params["oob_score"]
+#     class_weight = algo_params["class_weight"]
+#     classifier = RandomForestClassifier(n_estimators = n_estimators, 
+#                                         oob_score = oob_score, 
+#                                         class_weight = class_weight)
+        
+#     print "Train model"
+#     trained_classifier = train_model(train_file, image_file, classifier)
+    
+#     print "Classify"
+#     labels, scores, priorities = classify_w_scores(target_file, image_file, 
+#                                                    trained_classifier)
+    
+#     print "Write results"    
+#     values = zip(labels, scores, priorities)
+#     jt.write_values_to_geojson(values, 
+#                                ['class_name', 'score', 'tomnod_priority'], 
+#                                target_file, 
+#                                output_file)
+
+#     # Compute confusion matrix; this makes sense only if the target file
+#     # contains known labels
+#     print "Confusion matrix"
+#     C = jt.confusion_matrix_two_geojsons(target_file, output_file)
+#     print C
+
+#     print "Normalized confusion matrix"
+#     print C.astype(float)/C.sum(1)[:, None]
+
+#     print "Done!"
+    
+
+
