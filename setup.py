b231d675e22105a31905298e88ebed920a0390c9
Ubuntu <ubuntu@ip-172-30-1-218.ec2.internal>
2016-03-25 18:18:20 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ if sys.version_info > (3,):
 # long_description=readme,
       
 setup(name='mltools',
-      version='1.0.2',
-      description='Override feature_extractor when PolygonClassifier is instantiated. This allows flexibility to define feature extraction vector based on use case.',
+      version='1.0.3',
+      description='Handle exceptions in PolygonClassifier and data_extractors',
       classifiers=[],
       keywords='',
       author='Kostas Stamatiou',
