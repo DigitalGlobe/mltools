315047f743abb31e244341fdf3a558650a91680a
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-05-20 16:25:52 -0600

    add data generator to data_extractors for training large datasets

--- mltools/data_extractors.py
+++ mltools/data_extractors.py
@@ -12,23 +12,23 @@ def get_data(shapefile, return_labels=False, buffer=[0,0], mask=False):
        The image reference for each geometry is found in the image_id
        property of the shapefile.
        If shapefile contains points, then buffer must have non-zero entries.
-       The function also returns a list of geometry ids; this is useful in 
+       The function also returns a list of geometry ids; this is useful in
        case some of the shapefile entries do not produce a valid intensity
        array and/or class name.
-           
+
        Args:
            shapefile (str): Name of shapefile in mltools geojson format.
            return_labels (bool): If True, then a label vector is returned.
-           buffer (list): 2-dim buffer in PIXELS. The size of the box in each 
+           buffer (list): 2-dim buffer in PIXELS. The size of the box in each
                           dimension is TWICE the buffer size.
            mask (bool): Return a masked array.
-           
+
        Returns:
            chips (list): List of pixel intensity numpy arrays.
-           ids (list): List of corresponding geometry ids.    
+           ids (list): List of corresponding geometry ids.
            labels (list): List of class names, if return_labels=True
     """
- 
+
     data = []
 
     # go through point_file and unique image_id's
@@ -45,12 +45,12 @@ def get_data(shapefile, return_labels=False, buffer=[0,0], mask=False):
                                                 filter=[{'image_id':image_id}],
                                                 buffer=buffer,
                                                 mask=mask):
-            
+
             if chip is None or reduce(lambda x, y: x*y, chip.shape)==0:
                 continue
-            
-            this_data = [chip, properties['feature_id']] # every geometry must have id    
-           
+
+            this_data = [chip, properties['feature_id']] # every geometry must have id
+
             if return_labels:
                 try:
                     label = properties['class_name']
@@ -58,11 +58,72 @@ def get_data(shapefile, return_labels=False, buffer=[0,0], mask=False):
                         continue
                 except (TypeError, KeyError):
                     continue
-                this_data.append(label)             
+                this_data.append(label)
 
-            data.append(this_data) 
+            data.append(this_data)
 
     return zip(*data)
+
+
+def get_iter_data(shapefile, batch_size=32, min_chip_hw=100, max_chip_hw=224, return_labels=True, buffer=[0,0], mask=True):
+    '''
+    Generates batches of training data from shapefile for when it will not fit in memory.
+
+    INPUT   (1) string 'shapefile': name of shapefile to extract polygons from
+            (2) int 'batch_size': number of chips to generate per iteration. equal to batch-size of net, defaults to 32
+            (3) int 'min_chip_hw': minimum size acceptable (in pixels) for a polygon. defaults to 100
+            (4) int 'max_chip_hw': maximum size acceptable (in pixels) for a polygon. note that this will be the size of the height and width of input images to the net (default = 224)
+            (5) bool 'return_labels': return class label with chips. defaults to True
+            (6) list[int] 'buffer': two-dim buffer in pixels. defaults to [0,0].
+            (7) bool 'mask': if True returns a masked array. defaults to True
+
+    OUTPUT  (1) chips: one batch of masked (if True) chips
+            (2) corresponding feature_id for chips
+            (3) corresponding chip labels (if True)
+    '''
+
+    ct, data = 0, []
+    print 'Extracting image ids...'
+    img_ids = gt.find_unique_values(shapefile, property_name='image_id')
+
+    print 'Generating batch...'
+    for img_id in img_ids:
+        img = geoio.GeoImage(img_id + '.tif')
+
+        for chip, properties in img.iter_vector(vector=shapefile,
+                                                properties=True,
+                                                filter=[{'image_id':img_id}],
+                                                buffer=buffer,
+                                                mask=mask):
+
+
+            # check for adequate chip size
+            chan, h, w = np.shape(chip)
+            if chip is None or min(h, w) < min_chip_hw or max(h, w) > max_chip_hw:
+                continue
+
+            # zero-pad chip to standard net input size
+            chip = chip.filled(0) # replace masked entries with zeros
+            chip_patch = np.pad(chip, [(0,0), (0, max_chip_hw - h), (0, max_chip_hw - w)], 'constant', constant_values = 0)
+
+            this_data = [chip_patch, properties['feature_id']]
+
+            if return_labels:
+                try:
+                    label = properties['class_name']
+                    if label is None:
+                        continue
+                except (TypeError, KeyError):
+                    continue
+                this_data.append(label)
+            data.append(this_data)
+            ct += 1
+
+            if ct == batch_size:
+                yield zip(*data)
+                ct, data = 0, []
+
+    yield zip(*data)
 
 
 def random_window(image, chip_size, no_chips=10000):
@@ -72,7 +133,7 @@ def random_window(image, chip_size, no_chips=10000):
            image (str): Image filename.
            chip_size (list): Array of chip dimensions.
            no_chips (int): Number of chips.
-       
+
        Returns:
            List of chip rasters.
     """
