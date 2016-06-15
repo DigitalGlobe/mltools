5bb49143aa70d3c11bbe84a254cb3fc721e50815
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-06-15 10:56:25 -0600

    rename labels to allow multiple iterations get_iter_data

--- mltools/data_extractors.py
+++ mltools/data_extractors.py
@@ -161,6 +161,7 @@ def get_iter_data(shapefile, batch_size=32, nb_classes=2, min_chip_hw=30,
             if ct == batch_size:
                 l = [1 if lab == 'Swimming pool' else 0 for lab in labels]
                 lab = np_utils.to_categorical(l, nb_classes)
+                ct, inputs, labels, ids = 0, [], [], []
                 if return_id:
                     yield (np.array([i for i in inputs]), ids, lab)
                 else:
@@ -170,6 +171,7 @@ def get_iter_data(shapefile, batch_size=32, nb_classes=2, min_chip_hw=30,
     if len(inputs) != 0:
         l = [1 if lab == 'Swimming pool' else 0 for lab in labels]
         lab = np_utils.to_categorical(l, 2)
+        ct, inputs, labels, ids = 0, [], [], []
         if return_id:
             yield (np.array([i for i in inputs]), ids, np.array(lab))
         else:
